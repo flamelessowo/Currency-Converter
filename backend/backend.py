6b3b20448847a2d1b83e5fa62472a17e6795787f
flamelessowo <78107022+flamelessowo@users.noreply.github.com>
2022-03-12 23:18:51 +0200

    added currency api python file, also made endpoint for currency convertian and configured cors

--- backend/backend.py
+++ backend/backend.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import api
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:8080",
+]
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/convert")
+async def convert(amount: str, fromCurrency: str, to: str):
+    return api.convert_currency(amount, fromCurrency, to)
